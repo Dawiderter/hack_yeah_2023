994962e817ebce6ebe470cddd8b36e648df24e04
Kacegz <90635502+Kacegz@users.noreply.github.com>
2023-09-30 20:00:43 +0200

    Revamp pause

--- scenes/ui/MainMenu.gd
+++ scenes/ui/MainMenu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/world/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/player/player.tscn")
 
 func _on_options_pressed():
 	pass # Replace with function body.
--- scenes/ui/pause.gd
+++ scenes/ui/pause.gd
@@ -1,19 +1,23 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	process_mode = Node.PROCESS_MODE_PAUSABLE
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	hide()
+
 func _process(delta):
 	pass
 
-func _on_pause_button_pressed():
-	get_tree().paused = true
-	$pause_popup.show()
-	
-func _on_pause_popup_close_pressed():
-	$pause_popup.hide()
+func _input(event):
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().paused = true
+		show()
+
+func _on_Enter_button_down():
 	get_tree().paused = false
+	hide()
+
+
+func _on_continue_pressed():
+	get_tree().paused = false
+	hide()
