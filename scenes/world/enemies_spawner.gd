cdbb3c1f4db1a303a3a09a9f9dfae497a59047f9
Antlion931 <AlbertK931@protonmail.com>
2023-09-30 16:55:44 +0200

    Fixed bug that enemies didn't spawn outside camera

--- scenes/world/enemies_spawner.gd
+++ scenes/world/enemies_spawner.gd
@@ -1,20 +1,21 @@
-extends Node
+extends Node2D
 
 @export var enemy_1_scene: PackedScene 
+@export var player: Node2D
 
-func _on_start_delay_timeout():
-	$enemy_1_spawner_timer.start()
+func _process(delta):
+	$player_position.position = player.position
+	if Input.is_action_pressed("spawn_test"):
+		spawn_enemy_1()
 
-
-
-func _on_enemy_1_spawner_timer_timeout():
+func spawn_enemy_1():
 	var enemy = enemy_1_scene.instantiate()
 	var enemy_location = get_node("player_position/enemy_path/enemy_spawn_location")
 	enemy_location.progress_ratio = randf()
 
 	enemy.target = $player_position
 
-	enemy.position = enemy_location.position
+	enemy.global_position = enemy_location.global_position
 
 	add_child(enemy)
 
