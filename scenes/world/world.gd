6dc01736198a5fb6fd52d9ca7e12b902eb175884
Grubre <ogrodowczykjakub@gmail.com>
2023-10-01 01:34:12 +0200

    Player now spawns on random forest tile

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -5,8 +5,13 @@ extends Node2D
 @export var rows: int = 400
 @export var columns: int = 400
 
+@onready var level = $Level
+@onready var player = $player
+
 func _ready():
-	pass
+	var player_starting_point = level.generate_starting_position()
+	print(player_starting_point)
+	player.position = player_starting_point
 
 func _process(delta):
 	pass
