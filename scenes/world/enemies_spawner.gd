7d0a1d427a45f14b367af21c22908773e7f278ce
Grubre <ogrodowczykjakub@gmail.com>
2023-10-01 11:09:12 +0200

    Changed timer to 10 minutes, now enemies spawn in waves

--- scenes/world/enemies_spawner.gd
+++ scenes/world/enemies_spawner.gd
@@ -6,17 +6,33 @@ extends Node2D
 
 var current_time = 895
 
+var enemy_low = 0
+var enemy_high = 1
+
 func _process(delta):
+	$player_position.position = player.position
+	
+	if current_time == 400:
+		enemy_low = 1
+		enemy_high = 2
+	if current_time == 200:
+		enemy_low = 2
+		enemy_high = 3
+	
 	var time_left = timer.time_left
 	if current_time > ceil(time_left):
-		spawn_enemy(0)
+		spawn_enemy(enemy_low)
 		spawn_enemy(0)
 		current_time = ceil(time_left) - 1
 	
-	if int(ceil(time_left - 1)) % 20 == 0:
-		var count = ceil(log(900 - ceil(time_left)))
-		for i in range(count * count):
-			spawn_enemy(1)
+	if int(ceil(current_time - 1)) % 20 == 0:
+		var count = ceil(600 - ceil(current_time))
+		print(count)
+		for i in range(count):
+			spawn_enemy(enemy_high - 1)
+		for i in range(count / 2):
+			spawn_enemy(enemy_high)
+		current_time = current_time - 1
 
 func spawn_enemy(number):
 	var enemy = enemy_scenes[number].instantiate()
