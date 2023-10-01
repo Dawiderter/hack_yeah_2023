cedeabbc230e390db453824e9e08ee6c733f1bae
dawiderter <dwalczak456@gmail.com>
2023-10-01 05:25:23 +0200

    Bront

---
+++ scenes/player/dinos/bront.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@export var player_keep_range: float
+
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var attack : PackedScene = preload("res://scenes/player/dinos/bront_attack.tscn")
+@onready var cooldown : Timer = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+	if direction.x > 0:
+		sprite.flip_h = true
+	elif direction.x < 0:
+		sprite.flip_h = false
+
+	var during_attack = sprite.is_playing() && sprite.animation == "attack"
+			
+	if !during_attack:
+		sprite.offset.x = 0
+		if direction.is_zero_approx():
+			sprite.play("default")
+		else:
+			sprite.play("move")
+			
+	if !direction.is_zero_approx():
+		position = position.lerp(-direction * player_keep_range, 0.01)
+
+func play_attack():
+	sprite.offset.x = 16 * (-1 if sprite.flip_h else 1)
+	sprite.play("attack")
+	await sprite.animation_finished
+	var new_attack = attack.instantiate()
+	get_tree().root.add_child(new_attack)
+	new_attack.position = global_position 
+	new_attack.position += Vector2.RIGHT * (-1 if sprite.flip_h else 1) * 30
+	new_attack.start_wave(Vector2.RIGHT * (-1 if sprite.flip_h else 1))
+	
+func _on_timer_timeout():
+	play_attack()
---
+++ scenes/player/dinos/bront_attack.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var lifetime : Timer = $lifetime
+@onready var sprite : Sprite2D = $Sprite2D
+
+const LIFETIME: float = 2
+
+var scale_speed: float = 5
+var move_spped: float = 20
+var direction: Vector2 = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_wave(Vector2.DOWN)
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if direction != Vector2.ZERO:
+		scale += Vector2.ONE * delta * scale_speed
+		sprite.modulate.a = lerp(0,1,lifetime.time_left / LIFETIME)
+		position += direction * delta * move_spped 
+
+func _on_lifetime_timeout():
+	queue_free()
+
+func start_wave(dir: Vector2):
+	lifetime.start(LIFETIME)
+	direction = dir
+	rotation = dir.angle()
