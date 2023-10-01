10a6d95b178ab119f71ac1ca608bf23228d26e3d
dawiderter <dwalczak456@gmail.com>
2023-10-01 02:49:13 +0200

    Hit changes and knockbacks

--- scenes/hit/hurtbox.gd
+++ scenes/hit/hurtbox.gd
@@ -4,10 +4,23 @@ extends Area2D
 
 @export var hit_data: HitStats
 
+@onready var timer: Timer = $intensity
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	timer.wait_time = hit_data.intensity_seconds
+	timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_intensity_timeout():
+	if !hit_data.damage_on_enter:
+		deal_damage_to_all()
+
+func deal_damage_to_all():
+	for area in get_overlapping_areas():
+		if area.name == "hitbox":
+			area.get_hit(hit_data, self)
+
+func _on_area_entered(area:Area2D):
+	if hit_data.damage_on_enter:
+		if area.name == "hitbox":
+			area.get_hit(hit_data, self)
